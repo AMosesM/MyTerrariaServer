97c2a8bb6204d919a7a9a4d75041ac70dce6f114
Moses Molina <arnulfo.m.molina@gmail.com>
2023-05-20 11:22:16 -0700

    feat: runtime config

--- Makefile
+++ Makefile
@@ -1,32 +1,9 @@
 export TERRARIA_VERSION = 1.4.4.9
 export PYTHONDONTWRITEBYTECODE=1
-export PORT=7777
-
-# Google Cloud
-LOCATION:=us-central1
-PROJECT:=myterrariaserver
-REPOSITORY:=cloud-run-source-deploy
-IMAGE:=my-terraria-server
+export PORT=7778
 
 # Docker
 DOCKER_TAG:=${PROJECT}-${TERRARIA_VERSION}
-
-.PHONY: gcloud
-gcloud:
-	gcloud run deploy ${IMAGE} --allow-unauthenticated --source .
-
-.PHONY: gclean
-gclean:
-	gcloud run services delete ${IMAGE} --quiet
-	gcloud artifacts docker images delete ${LOCATION}-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE} --delete-tags --quiet
-
-.PHONY: reload
-reload: gclean gcloud
-	echo done.
-
-.PHONY: reqs
-reqs:
-	python3 -m pip install -r requirements.txt
 
 .PHONY: localhost
 localhost: export APP_HOME=${PWD}
@@ -39,7 +16,7 @@ server:
 
 .PHONY: build
 build: server
-	docker build -t ${DOCKER_TAG} .
+	docker build  -t ${DOCKER_TAG} .
 
 .PHONY: rm_container
 rm_container:
--- run_terraria_server.py
+++ run_terraria_server.py
@@ -1,21 +1,40 @@
 import os
 import sys
 import subprocess
+import re
 
 SERVER_INSTALL = "{APP_HOME}/server/{TERRARIA_VERSION}".format(**os.environ) + f"/{os.environ.get('TERRARIA_VERSION').replace('.','')}"
-print(f"{sys.platform}")
+WORLD_PATH = "{APP_HOME}/Worlds/".format(**os.environ)
+WORLD_FILE = "{WORLD_PATH}/MyTerrariaServer.wld".format(**locals())
+PORT = os.environ.get("PORT", "7777")
+SERVER_CONFIG = "{APP_HOME}/server.cfg".format(**os.environ)
+SERVER_RUNTIME_CONFIG = "{APP_HOME}/server-runtime.cfg".format(**os.environ)
+
+def create_runtime_config():
+    with open(SERVER_CONFIG, "r") as cfg:
+        cfg_text = cfg.read()
+        cfg_text = re.sub("worldpath=.*",f"worldpath={WORLD_PATH}", cfg_text)
+        cfg_text = re.sub("world=.*",f"world={WORLD_FILE}", cfg_text)
+        cfg_text = re.sub("port=.*",f"port={PORT}", cfg_text)
+        with open(SERVER_RUNTIME_CONFIG, "w") as runtime_cfg:
+            runtime_cfg.write(cfg_text)
+
+create_runtime_config()
+
 if sys.platform == "linux" or sys.platform == "linux2":
     # linux
-    pass
+    subprocess.call([
+        f"{SERVER_INSTALL}/Linux/TerrariaServer",
+        "-config", SERVER_RUNTIME_CONFIG
+    ])
+    
 elif sys.platform == "darwin":
     print("starting server for mac")
     print(os.environ.get("APP_HOME"))
     # OS X
     subprocess.call([
         f"{SERVER_INSTALL}/Mac/Terraria Server.app/Contents/MacOS/TerrariaServer",
-        "-world", "~/world.wld".format(**os.environ),
-        "-autocreate", "1"
-        
+       "-config", SERVER_RUNTIME_CONFIG
     ])
 elif sys.platform == "win32":
     # Windows...
