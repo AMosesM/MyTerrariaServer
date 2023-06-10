682ff329d2e07f2a3351bf174d8f6aa832fb7843
Moses Molina <arnulfo.m.molina@gmail.com>
2023-06-10 09:49:38 -0700

    chore: syntax/config cleanup

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,6 @@ ENV PYTHONDONTWRITEBYTECODE=1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
 
-# Install pip requirements
-COPY requirements.txt .
-RUN python -m pip install -r requirements.txt
-
 # Copy local code to the container image.
 ENV APP_HOME=/app
 WORKDIR $APP_HOME
--- download_terraria_server.py
+++ download_terraria_server.py
@@ -2,13 +2,14 @@ import os
 import subprocess
 
 TERRARIA_VERSION = os.environ.get("TERRARIA_VERSION")
-
 SERVER_ZIP = f"terraria-server-{TERRARIA_VERSION.replace('.','')}.zip"
 DOWNLOAD_URL = f"https://terraria.org/api/download/pc-dedicated-server/{SERVER_ZIP}"
 ROOT = "server"
 SERVER_INSTALL = f"{ROOT}/{TERRARIA_VERSION}"
+
 if not os.path.exists(SERVER_ZIP):
     subprocess.call(["wget", DOWNLOAD_URL])
+
 if not os.path.exists(SERVER_INSTALL):
     os.makedirs(ROOT, exist_ok=True)
     subprocess.call(["unzip", SERVER_ZIP, "-d", SERVER_INSTALL])
